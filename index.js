a4eb7f911b74513c095570d5e723ba56eac2ab30
BuildTools <unconfigured@null.spigotmc.org>
2017-04-05 04:19:45 +0000

    randomness achieved

--- index.js
+++ index.js
@@ -2,25 +2,26 @@ var SerialPort = require('serialport');
 var rpio = require('rpio');
 // Enough pins to generate two octals per instant.
 var pins = [7, 11, 12, 13, 15, 16, 18, 22, 29, 31, 32, 33, 35, 36, 37, 38];
-var pinsReady = 0;
-/*process.on('SIGINT', function () {
-	for (var i = 0; i < pins.length; i++) {
-		rpio.close(pins[i]);
-	}
-});*/
 for (var i = 0; i < pins.length; i++) {
-	rpio.open(pins[i], rpio.INPUT);
+	rpio.open(pins[i], rpio.INPUT, rpio.PULL_OFF);
 }
 setInterval(function () {
 	var output = 0;
 	var digitValue = 1;
-	var pinsRead = 0;
+	var possiblePins = [];
+	for (var i = 0; i < pins.length; i++) {
+		possiblePins.push(i);
+	}
+	var selectedPins = [];
 	for (var i = 0; i < 8; i++) {
-		if (rpio.read(pins[i])) output += digitValue;
+		selectedPins.push(pins[possiblePins.splice(Math.floor(Math.random() * possiblePins.length), 1)[0]]);
+	}
+	for (var i = 0; i < 8; i++) {
+		if (rpio.read(selectedPins[i])) output += digitValue;
 		digitValue *= 2;
 	}
 	console.log(output);
-}, parseInt(process.env.PIN_DELAY) || 1000);
+}, parseInt(process.env.PIN_DELAY) || 10);
 
 var size = [parseInt(process.env.WIDTH) || 8, parseInt(process.env.HEIGHT) || 8];
 var image = [];
